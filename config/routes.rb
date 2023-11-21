d8f0635ed7ac61283e53c7c4bfdf311adb30a202
Aditya Lahiri <adityalahiri13@gmail.com>
2023-11-22 00:47:06 +0530

    Add model for messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "pages#home"
+  get '/messages', to: 'messages#index'
+  get '/messages/new', to:'messages#new'
+  post 'messages', to:'messages#create'
   # Defines the root path route ("/")
   # root "articles#index"
 end
